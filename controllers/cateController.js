6e3b71155e76fd40ec79785b232623d69fa0103e
nhut hao <ngonhuthao912004@gmail.com>
2024-10-08 12:11:50 +0700

    hao api 8/10/2024

--- controllers/cateController.js
+++ controllers/cateController.js
@@ -1,5 +1,5 @@
+const { query } = require("express");
 const Cate = require("../models/cate");
-const upload = require("../config/update");
 
 // Lấy tất cả danh mục
 exports.getAllCates = async (req, res) => {
@@ -11,25 +11,20 @@ exports.getAllCates = async (req, res) => {
   }
 };
 
+
+
 // Hàm xử lý việc thêm danh mục với hình ảnh
 exports.addCate = async (req, res) => {
   try {
-    upload.single("hinh_anh")(req, res, async (err) => {
-      //nếu có lỗi khi upload file thì trả
-      if (err) {
-        return res.status(400).json({ error: err.message });
-      }
-      const { danh_muc, mo_ta } = req.body;
-      const hinh_anh = req.file ? req.file.originalname : "";
-      const cate = new Cate({ danh_muc, mo_ta, hinh_anh });
-      await cate.save();
-      res.json(cate);
-    });
+    const { danh_muc, mo_ta } = req.body;
+    const hinh_anh = req.file ? req.file.originalname : ""; // Get the uploaded file
+    const cate = new Cate({ danh_muc, mo_ta, hinh_anh });
+    await cate.save();
+    res.json(cate);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
-
 
 // Hàm xử lý việc xóa danh mục
 exports.deleteCate = async (req, res) => {
@@ -48,25 +43,18 @@ exports.deleteCate = async (req, res) => {
 // Hàm xử lý việc cập nhật danh mục
 exports.updateCate = async (req, res) => {
   try {
-    // Tìm danh mục theo ID
     const cate = await Cate.findOne({ where: { _id: req.params.id } });
     if (!cate) {
       return res.status(404).json({ error: "Không tìm thấy danh mục" });
     }
-    // Xử lý upload ảnh
-    upload.single("hinh_anh")(req, res, async (err) => {
-      if (err) {
-        return res.status(400).json({ error: err.message });
-      }
-      // Cập nhật dữ liệu
-      const { danh_muc, mo_ta } = req.body;
-      cate.danh_muc = danh_muc || cate.danh_muc;
-      cate.mo_ta = mo_ta || cate.mo_ta;
-      cate.hinh_anh = req.file ? req.file.originalname : cate.hinh_anh;
-      // Lưu thay đổi
-      await cate.save();
-      res.json(cate);
-    });
+
+    const { danh_muc, mo_ta } = req.body;
+    cate.danh_muc = danh_muc || cate.danh_muc;
+    cate.mo_ta = mo_ta || cate.mo_ta;
+    cate.hinh_anh = req.file ? req.file.originalname : cate.hinh_anh; // Only update image if file is uploaded
+
+    await cate.save();
+    res.json(cate);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
